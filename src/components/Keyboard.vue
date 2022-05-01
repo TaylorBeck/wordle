2e74ec42fe5cc7efc060c99848dbf0b60fb2a936
Taylor Beck <beck.taylorg@gmail.com>
2022-04-30 20:17:54 -0700

    Added keyboard component

---
+++ src/components/Keyboard.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="keyboard"></div>
+</template>
+
+<script setup>
+import Keyboard from "simple-keyboard";
+import "simple-keyboard/build/css/index.css";
+
+import { ref, onMounted } from "vue";
+
+const emit = defineEmits([
+  "onKeyPress"
+]);
+
+const keyboard = ref(null);
+
+const onKeyPress = (key) => {
+  emit("onKeyPress", key);
+};
+
+onMounted(() => {
+  keyboard.value = new Keyboard("keyboard", {
+    layout: {
+      default: [
+        "q w e r t y u i o p {bksp}",
+        "a s d f g h j k l {enter}",
+        "z x c v b n m"
+      ]
+    },
+    buttonTheme: [
+      {
+        class: "hg-green",
+        buttons: "{enter}"
+      },
+      {
+        class: "hg-black",
+        buttons: "{bksp}"
+      }
+    ],
+    onKeyPress: onKeyPress
+  })
+});
+</script>
+
+<style>
+</style>
